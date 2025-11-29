6ab2168d398a3359dafcca3f9036c03fa9f192ce
Arivusudar <contactsudar@gmail.com>
2025-11-29 18:38:42 +0530

    new online class

---
+++ src/classes/class_1764421408736.js
@@ -0,0 +1,42 @@
+// src/classes/class_1764421408736.js
+
+const class_1764421408736 = 
+  {
+    id: 1764421408736,
+    title: "Whipped Wonders: Fresh Cream Pastry Masterclass",
+    date: "25th December 2025",
+    details: `🎉  Join us for an exclusive workshop where you'll learn everything from perfecting pastry cream to mastering decorative techniques.
+👩‍🍳 *What You'll Learn:*
+- 🍰 Crafting delicious pastry bases
+- 🎨 Decorating with fresh cream and elegant designs
+- 💡 Tips and tricks for professional presentation
+Sponge cakes :
+vanilla sponge
+chocolate sponge( two different sponge )
+Flavours :
+Premium Pineapple Gateaux
+Black Forest Pastry cake
+Blueberry pastry cake
+Butter Scotch Delight with caramel filling
+Dutch truffle chocolate cake
+Icing :
+Fresh Cream Frosting
+Different types of filling
+Ganache icing
+Glaze icing
+Truffle icing &
+Decoration with chocolates
+📅 *Time:* 10 30 AM to 5 PM
+Fees:- Rs. 2800 per person
+📍 *Location:* 46/18, Gun Firing Street, Fort
+Salem, Tamil Nadu..
+Participants will receive the Certificate of Completion
+Plz bring containers to take baked goods..
+.
+For Registration please WhatsApp to 07810940789 or send SMS to 07810940789`,
+    time: " 10 30 AM to 5 PM",
+    price: "Rs. 2800/-",
+    type: "offline",
+  }
+
+export default class_1764421408736;
