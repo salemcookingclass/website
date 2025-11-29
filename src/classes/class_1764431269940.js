86e4d84e2529063f9d08ff7fa96291b3aa9fc3ca
Arivusudar <contactsudar@gmail.com>
2025-11-29 21:18:35 +0530

    new online class

---
+++ src/classes/class_1764431269940.js
@@ -0,0 +1,15 @@
+// src/classes/class_1764431269940.js
+
+const class_1764431269940 = 
+  {
+    id: 1764431269940,
+    title: "Gourmet Cookies",
+    date: "26 December 2025",
+    details: `Pre Recorded videos
+https://youtu.be/He0ofkuIjDA?si=H0axFd0bO8MMCdh8`,
+    time: "10 30 -  5:30",
+    price: "Rs. 2700 /-",
+    type: "online",
+  }
+
+export default class_1764431269940;
