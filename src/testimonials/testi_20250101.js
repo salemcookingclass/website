a67139dfc0d562717128dc66496ece1351c06c59
Arivusudar <contactsudar@gmail.com>
2025-11-30 14:47:24 +0530

    testi design

--- src/testimonials/testi_20250101.js
+++ src/testimonials/testi_20250101.js
@@ -1,4 +1,10 @@
 export default {
-  name: "Priya S",
-  feedback: "The best baking class ever! Very detailed and hands-on.",
+  name: "Shylajee Ladies Tailoring and Training Institute",
+  feedback: `Hi, iam Lakshmijagannathan, Salem cakecraft studio baking class gave me a amazing experience! .
+  The instructor was knowledgeable, patient, and provided constructive feedback. 
+  I learned various techniques, from basic to advanced, and enjoyed making delicious treats. 
+  The classroom was well-equipped, and the atmosphere was friendly and supportive. 
+  I highly recommend this class to anyone interested in baking!", 
+  today exotic work shop classes make me more confidence in this field.
+  Thankyou Ramya mam.🙏🏻`
 };
--- src/testimonials/testi_20250112.js
+++ src/testimonials/testi_20250112.js
@@ -1,4 +1,11 @@
 export default {
-  name: "John D",
-  feedback: "Amazing teaching style. Learned a lot!",
+  name: "Mithils By Keerthana",
+  feedback: `A place where you can trustedly go and learn, and come back home with not only takeaways 
+but a handful of hands-on experience and knowledge from basics.
+My journey as a home baker all started with Ramya and she teaches so well that even someone who doesn't 
+know baking can confidently bake a cake. I have almost 10 years of 
+learning with her and attended many classes with her. 
+Her teaching, methods, way of clearing doubts and after-support are simply amazing. 
+Even after years of learning, she replies to doubts just like she did in class... excellent teaching and the best place to learn.
+Keep going Ramya Akka...`
 };
